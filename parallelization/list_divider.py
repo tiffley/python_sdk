0f2aef254eda436312c63ab2cc5c34f1c94ff33c
Yamauchi, Takaakira | Taka | DPD <takaakira.yamauchi@rakuten.com>
2023-12-06 23:59:55 +0900

    add list divider

--- parallelization/list_divider.py
+++ parallelization/list_divider.py
@@ -6,8 +6,8 @@ def list_divider(li, div_cnt=4) -> list[list]:
     """
     chunk_size = len(li) // div_cnt  # Determine the size of each sublist
     sublists = [li[i:i + chunk_size] for i in range(0, len(li), chunk_size)]
-    if len(sublists) > 4:
-        diff = len(sublists) - 4
+    if len(sublists) > div_cnt:
+        diff = len(sublists) - div_cnt
         for i in range(1, diff + 1):
             sublists[-i - 1].extend(sublists.pop())
     return sublists
